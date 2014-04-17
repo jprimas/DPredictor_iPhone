a7f20ec87ce9d4feb8dadd102264ba56411d4962
Joshua Primas <JoshuaPrimas@gmail.com>
2014-04-17 11:20:02 -0700

    Initial Commit

---
+++ DPredictor/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DPredictor
+//
+//  Created by Joshua Primas on 4/17/14.
+//  Copyright (c) 2014 Joshua Primas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DPredictorTests/DPredictorTests.m
@@ -0,0 +1,34 @@
+//
+//  DPredictorTests.m
+//  DPredictorTests
+//
+//  Created by Joshua Primas on 4/17/14.
+//  Copyright (c) 2014 Joshua Primas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DPredictorTests : XCTestCase
+
+@end
+
+@implementation DPredictorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
